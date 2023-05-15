2849d22e5b1c2eb4b68f9b2b6141dc5673268f4a
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2023-05-15 19:46:48 -0300

    atualizando projeto

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-const {Builder} = require('selenium-webdriver');
-require("chromedriver");
+const { Builder, By, Key } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-(async function helloSelenium() {
-  let driver = await new Builder().forBrowser('chrome').build();
+let options = new chrome.Options();
+options.addArguments('--disable-dev-shm-usage');
 
-  await driver.get('https://selenium.dev');
+let driver = new Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build();
 
-  //await driver.quit();
-})();
+async function run() {
+  try {
+    await driver.get('https://google.com');
+    const searchBar = await driver.findElement(By.name('q'));
+    await searchBar.sendKeys('nodejs', Key.RETURN);
+  } finally {
+    //await driver.quit();
+  }
+}
+
+run();
